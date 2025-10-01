27211cfa7744c9f6ad4c1e893dc594c9f201719f
2dam <rvm666@educa.madrid.org>
2025-10-01 13:13:43 +0200

    nose organizar bien y hacer el banco online

--- src/main/java/Hilos/FoodTrack/FoodTrackService.java
+++ src/main/java/Hilos/FoodTrack/FoodTrackService.java
@@ -1,11 +1,26 @@
 package Hilos.FoodTrack;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.concurrent.ArrayBlockingQueue;
 import java.util.concurrent.BlockingQueue;
 import java.util.concurrent.LinkedBlockingQueue;
 
 public class FoodTrackService {
+    private BlockingQueue<Pedido> pedidosPendientes;
+    private List<Thread> cocineros;
 
-    BlockingQueue<Pedido> pedidosPendientes = new ArrayBlockingQueue<>(10);
+    public FoodTrackService() {
+        this.pedidosPendientes = new ArrayBlockingQueue<>(10);
+        this.cocineros = new ArrayList<>();
+        for (int i = 0; i < 3; i++) {
+            Thread cocinero  = new Thread();
+            cocineros.add(cocinero);
+            cocinero.start();
+        }
+    }
+
+
+
 
 }
