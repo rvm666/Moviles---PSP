235958d7697911aa96e292fa394d56c35cb405d2
2dam <rvm666@educa.madrid.org>
2025-10-02 11:49:55 +0200

    calendairi ohecho pero no funciona

--- app/src/main/java/com/example/anadirusuarios1/MainActivity.kt
+++ app/src/main/java/com/example/anadirusuarios1/MainActivity.kt
@@ -19,6 +19,7 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
+        binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
