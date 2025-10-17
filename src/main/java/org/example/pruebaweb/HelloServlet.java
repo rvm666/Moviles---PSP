b1a6d9573a92fd3c9d9120b1777bb4b4add1a8bf
rvm666 <rvm666@educa.madrid.org>
2025-10-17 23:58:49 +0200

    initial

--- src/main/java/org/example/pruebaweb/HelloServlet.java
+++ src/main/java/org/example/pruebaweb/HelloServlet.java
@@ -2,27 +2,22 @@ package org.example.pruebaweb;
 
 import java.io.*;
 
+import jakarta.servlet.ServletException;
 import jakarta.servlet.http.*;
 import jakarta.servlet.annotation.*;
 
-@WebServlet(name = "helloServlet", value = "/hello-servlet")
+@WebServlet("/hola")
 public class HelloServlet extends HttpServlet {
-    private String message;
 
-    public void init() {
-        message = "Hello World!";
+    @Override
+    public void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        var contador = 1;
+        if (request.getSession().getAttribute("contador") != null) {
+            contador = (Integer) request.getSession().getAttribute("contador");
+        }
+        response.getWriter().write("HOLA MUNDO " + contador);
+        request.getSession().setAttribute("contador", contador+1);
+
     }
 
-    public void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException {
-        response.setContentType("text/html");
-
-        // Hello
-        PrintWriter out = response.getWriter();
-        out.println("<html><body>");
-        out.println("<h1>" + message + "</h1>");
-        out.println("</body></html>");
-    }
-
-    public void destroy() {
-    }
 }
---
+++ src/main/java/org/example/pruebaweb/HolaMundoHtml.java
@@ -0,0 +1,9 @@
+package org.example.pruebaweb;
+
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+
+@WebServlet("/html")
+public class HolaMundoHtml extends HttpServlet {
+
+}
