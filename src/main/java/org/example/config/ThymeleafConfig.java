3d9caaf2ac4e14c2e11336a88843811218fcc42d
rvm666 <rvm666@educa.madrid.org>
2025-10-18 11:59:47 +0200

    initial

---
+++ src/main/java/org/example/config/ThymeleafConfig.java
@@ -0,0 +1,30 @@
+package org.example.config;
+
+import jakarta.servlet.ServletContext;
+import jakarta.servlet.ServletContextEvent;
+import jakarta.servlet.ServletContextListener;
+import jakarta.servlet.annotation.WebListener;
+import org.thymeleaf.TemplateEngine;
+import org.thymeleaf.templateresolver.WebApplicationTemplateResolver;
+import org.thymeleaf.web.servlet.JakartaServletWebApplication;
+
+@WebListener
+public class ThymeleafConfig implements ServletContextListener {
+
+    @Override
+    public void contextInitialized(ServletContextEvent sce) {
+        ServletContext servletContext = sce.getServletContext();
+        JakartaServletWebApplication application  = JakartaServletWebApplication.buildApplication(servletContext);
+
+        WebApplicationTemplateResolver templateResolver = new WebApplicationTemplateResolver(application);
+        templateResolver.setPrefix("/WEB-INF/templates/");
+        templateResolver.setSuffix(".html");
+        templateResolver.setTemplateMode("HTML");
+        templateResolver.setCharacterEncoding("UTF-8");
+
+        TemplateEngine templateEngine = new TemplateEngine();
+        templateEngine.setTemplateResolver(templateResolver);
+
+        servletContext.setAttribute("com.daw.TemplateEngine", templateEngine);
+    }
+}
