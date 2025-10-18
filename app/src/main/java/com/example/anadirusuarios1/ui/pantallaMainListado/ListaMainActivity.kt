a01809f9efa4ee896eaab3b96223aa6b65d197ac
rvm666 <rvm666@educa.madrid.org>
2025-10-18 19:51:37 +0200

    Hecho bien el xml

--- app/src/main/java/com/example/anadirusuarios1/ui/pantallaMainListado/ListaMainActivity.kt
+++ app/src/main/java/com/example/anadirusuarios1/ui/pantallaMainListado/ListaMainActivity.kt
@@ -1,17 +1,27 @@
 package com.example.anadirusuarios1.ui.pantallaMainListado
 
 import android.os.Bundle
+import androidx.activity.ComponentActivity
 import androidx.activity.enableEdgeToEdge
+import androidx.activity.viewModels
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 import com.example.anadirusuarios1.R
+import com.example.anadirusuarios1.databinding.ActivityListaMainBinding
 
-class ListaMainActivity : AppCompatActivity() {
+class ListaMainActivity : ComponentActivity() {
+
+    private val ViewModel: ListaProduccionViewModel by viewModels()
+
+    private lateinit var adapter: ProduccionAdapter
+
+    private lateinit var binding: ActivityListaMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        binding = ActivityListaMainBinding.inflate(layoutInflater)
         enableEdgeToEdge()
-        setContentView(R.layout.activity_lista_main)
+        setContentView(binding.root)
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
