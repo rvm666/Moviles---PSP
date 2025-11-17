16b00bf806b69e61c821ee4d986fb9e2c709697c
rvm666 <rvm666@educa.madrid.org>
2025-11-17 09:52:32 +0100

    nose

---
+++ PSP/RestSpring/src/test/java/org/example/restspring/RestSpringApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.restspring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RestSpringApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
